dfa9b117d2ed0687aeae855cc57a3e7e1076b346
MikeVrnd <114433957+MikeVrnd@users.noreply.github.com>
2025-11-15 21:05:17 +0200

    Fix: use relative audio paths and import validator

--- src/PerformanceHud.jsx
+++ src/PerformanceHud.jsx
@@ -1,32 +1,32 @@
 // PerformanceHud.jsx
 import { Perf } from "r3f-perf";
-import Stats from "stats-js";
+// import Stats from "stats-js";
 import { useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
-import { Profiler } from "react";
+// import { Profiler } from "react";
 import { Html } from "@react-three/drei";
 
 /* -------------------------------
    Stats.js overlay (top-right)
 --------------------------------*/
-function StatsMonitor() {
-  useEffect(() => {
-    const stats = new Stats();
-    stats.showPanel(0);
-    stats.dom.style.position = "absolute";
-    stats.dom.style.top = "0px";
-    stats.dom.style.right = "0px";
-    stats.dom.style.zIndex = 9999;
-    document.body.appendChild(stats.dom);
+// function StatsMonitor() {
+//   useEffect(() => {
+//     const stats = new Stats();
+//     stats.showPanel(0);
+//     stats.dom.style.position = "absolute";
+//     stats.dom.style.top = "0px";
+//     stats.dom.style.right = "0px";
+//     stats.dom.style.zIndex = 9999;
+//     document.body.appendChild(stats.dom);
 
-    return () => {
-      document.body.removeChild(stats.dom);
-    };
-  }, []);
+//     return () => {
+//       document.body.removeChild(stats.dom);
+//     };
+//   }, []);
 
-  useFrame(() => {});
-  return null;
-}
+//   useFrame(() => {});
+//   return null;
+// }
 
 /* -------------------------------
    Frame time logger (bottom-left)
@@ -74,12 +74,12 @@ export default function PerformanceHud() {
       {/* <StatsMonitor position="top-right" /> */}
 
       {/* Custom frame logger (bottom-left) */}
-      <FrameLogger />
+      {/* <FrameLogger /> */}
 
       {/* React Profiler (bottom-right overlay) */}
-      <Profiler id="R3F-Scene" onRender={onRenderCallback}>
-        {/* This does NOT render UI, only measures */}
-        {/* <Html>
+      {/* <Profiler id="R3F-Scene" onRender={onRenderCallback}> */}
+      {/* This does NOT render UI, only measures */}
+      {/* <Html>
           <div
             style={{
               position: "absolute",
@@ -95,7 +95,7 @@ export default function PerformanceHud() {
             React Profiler Active
           </div>
         </Html> */}
-      </Profiler>
+      {/* </Profiler> */}
     </>
   );
 }
